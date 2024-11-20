fddc590541e0d77517e44a3eb2ca0e15fe1810c1
akash100a <arakash968786@gmail.com>
2024-11-20 20:10:37 +0600

    a

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
 const repoName = process.argv[2];
-const gitCheckoutCommand = `git clone --dept 1 `
+const gitCheckoutCommand = `git clone --dept 1 https://github.com/arakash-developer/next-starter.git`
