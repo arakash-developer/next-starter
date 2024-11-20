25db45f70227b08be6dfb2740be4cd403ee32657
akash100a <arakash968786@gmail.com>
2024-11-20 21:45:01 +0600

    k

--- bin/cli.js
+++ bin/cli.js
@@ -10,7 +10,7 @@ const runCommand = command =>{
     return true;
 }
 const repoName = process.argv[2];
-const gitCheckoutCommand = `git clone --dept 1 https://github.com/arakash-developer/next-starter ${repoName}`;
+const gitCheckoutCommand = `git clone --dept 1 https://github.com/arakash-developer/next-starter`;
 const installDepsCommand = `cd ${repoName} && npm install`;
 console.log(`Cloning the repository with name ${repoName}`);
 const checkedOut =  runCommand(gitCheckoutCommand);
