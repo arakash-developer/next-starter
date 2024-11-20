70c04975f43eb8e6e412a7b5188eb1bdab1e6ee5
akash100a <arakash968786@gmail.com>
2024-11-20 20:38:29 +0600

    a

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,22 @@
 #!/usr/bin/env node
+const {exeSync} = require('child_process');
+const runCommand = command =>{
+    try{
+        exeSync(`${command}`,{stdio: 'inherit'});
+    }catch(e){
+        console.error(`Failed to execute ${command}`,e);
+        return false;
+    }
+    return true;
+}
 const repoName = process.argv[2];
-const gitCheckoutCommand = `git clone --dept 1 https://github.com/arakash-developer/next-starter.git`
+const gitCheckoutCommand = `git clone --dept 1 https://github.com/arakash-developer/next-starter ${repoName}`;
+const installDepsCommand = `cd ${repoName} && npm install`;
+console.log(`Cloning the repository with name ${repoName}`);
+const checkedOut =  runCommand(gitCheckoutCommand);
+if(!checkedOut) process.exit(-1);
+console.log(`Installing dependencies for ${repoName}`);
+const installedDeps = runCommand(installDepsCommand);
+if(!installedDeps) process.exit(-1); 
+console.log(`Congratulation!! You are Ready`);
+
