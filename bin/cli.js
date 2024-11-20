74902efcc32147a9b1eb570613a97e2606748815
akash100a <arakash968786@gmail.com>
2024-11-20 23:06:46 +0600

    up

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 const {execSync} = require('child_process');
-const { log } = require('console');
 const runCommand = command =>{
     try{
         execSync(`${command}`,{stdio: 'inherit'});
@@ -14,12 +13,13 @@ const repoName = process.argv[2];
 console.log(repoName);
 const gitCheckoutCommand = `git clone --dept 1 https://github.com/arakash-developer/nextjs-starter ${repoName}`;
 const installDepsCommand = `cd ${repoName} && npm install`;
-console.log(`Cloning the repository with name ${repoName}`);
+// console.log(`Cloning the repository with name ${repoName}`);
+console.log(`Installing Packages for ${repoName} @akash.dev`);
 const checkedOut =  runCommand(gitCheckoutCommand);
 if(!checkedOut) process.exit(-1);
-console.log(`Installing dependencies for ${repoName}`);
+console.log(`Installing dependencies for ${repoName} @akash.dev`);
 const installedDeps = runCommand(installDepsCommand);
 if(!installedDeps) process.exit(-1); 
-console.log(`Congratulation!! You are Ready`);
-console.log(`cd ${repoName} && npm run dev or npm start`);
+console.log(`Congratulation!! You are Ready @akash.dev`);
+console.log(`cd ${repoName} && npm run dev or npm start @akash.dev`);
 
